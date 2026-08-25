611566784759d07a42967b3fb2453953f6be9e35
76mrmario <80854944+76mrmario@users.noreply.github.com>
2026-08-25 12:09:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ console.log("Kubix server starting...");
 var handler = function(request, response) {
     console.log( Date.now()+" Received request from "+ request.connection.remoteAddress );
     response.writeHead(200);
-    response.end(process.env.BEGRUESSUNG +"\nYou've hit "+os.hostname()+"\n" + "Now its " + Date.now()+"!\n");
+    response.end(process.env.BEGRUESSUNG +"\nYou've hit "+os.hostname()+"\n" + "Now its " + Date.now()+"!\nYour Commit Hash is :" + process.env.GIT_SHA);
 };
 var www = http.createServer(handler);
 www.listen(8080);
